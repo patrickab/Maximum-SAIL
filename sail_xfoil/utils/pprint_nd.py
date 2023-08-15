5ce9d8e3aa0dcc4189f43e0284b55f3b3b612db8
ru96xed <ru96xed@campus.lmu.de>
2023-08-15 13:26:52 +0200

    update folder structure in sail_xfoil

--- sail_xfoil/utils/pprint_nd.py
+++ sail_xfoil/utils/pprint_nd.py
@@ -1,7 +1,31 @@
 import numpy as np
 import inspect
 
-from utils.pprint import pprint
+
+def pprint(variable):
+
+    # Get the calling frame
+    frame = inspect.currentframe().f_back
+
+    # Find the variable name that was passed as an argument to the function
+    variable_name = None
+    for name, value in frame.f_locals.items():
+        if value is variable:
+            variable_name = name
+            break
+
+    if isinstance(variable, np.ndarray):
+        shape_string = str(variable.shape)
+
+    print("\n Name: " + variable_name)
+    print(" Type: " + str(type(variable)))
+    print("Shape: " + str(variable.shape))
+
+    print(variable_name + ": ")
+    print(variable)
+
+    return
+
 
 def pprint_nd(matrix1, matrix2, matrix3=None):
 
--- sail_xfoil/utils/utils.py
+++ sail_xfoil/utils/utils.py
@@ -1,9 +1,16 @@
+import os
+
+### Global Parameters ###
+from config.config import Config
+config = Config(os.path.join(os.path.dirname(__file__), '../config', 'config.ini'))
+SOL_VALUE_RANGE = config.SOL_VALUE_RANGE
+
+
 # elite_status_vector = archive.add(acq_elites, obj_evals, bhv_evals)
         # elite_status_vector == 0  ->  acq_elite was not added
         # elite_status_vector == 1  ->  acq_elite was added
         # elite_status_vector == 2  ->  acq_elite discovered new cell
-
-def select_new_elites(candidate_elites, obj_evals, elite_status_vector):
+def select_new_elites(elite_status_vector,candidate_elites, obj_evals):
 
     x_new_elites = []
     obj_new_elites = []
@@ -14,3 +21,14 @@ def select_new_elites(candidate_elites, obj_evals, elite_status_vector):
             obj_new_elites.append(obj_eval)
 
     return x_new_elites, obj_new_elites
+
+#def count_new_elites(elite_status_vector):
+
+
+def scale_samples(samples):
+    for i in range(len(samples)):
+        for j in range(len(samples[i])):
+            lower_bound, upper_bound = SOL_VALUE_RANGE[j]
+            samples[i][j] = samples[i][j] *(upper_bound - lower_bound) + lower_bound
+
+    return samples
