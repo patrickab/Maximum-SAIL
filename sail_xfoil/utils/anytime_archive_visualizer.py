73dc796eb5d7a2bbd59f6ecd25bf15bd835413e1
patrickab <patrick.a.bitzer@gmail.com>
2023-10-23 12:55:32 +0200

    Optimize Buffering Behavior for Archive Visualizer

--- sail_xfoil/utils/anytime_archive_visualizer.py
+++ sail_xfoil/utils/anytime_archive_visualizer.py
@@ -6,60 +6,166 @@ import subprocess
 import os
 import gc
 
-PNG_BUFFERSIZE = 10
+###### Configurable Variables ######
+from config.config import Config
+config = Config('config/config.ini')
+
+BATCH_SIZE = config.BATCH_SIZE
+ACQ_N_OBJ_EVALS = config.ACQ_N_OBJ_EVALS
+INIT_N_EVALS = config.INIT_N_EVALS
+MAX_PRED_VERIFICATION = config.MAX_PRED_VERIFICATION
 
 
-def anytime_archive_visualizer(self, archive):
+# eg for n_obj_evals=1000, and BATCH_SIZE=10, we buffer 100/4 = 25 pngs
+n_obj_evals = INIT_N_EVALS + ACQ_N_OBJ_EVALS + MAX_PRED_VERIFICATION
+PNG_BUFFERSIZE = (n_obj_evals/BATCH_SIZE) / 8
 
-    iteration = self.current_iteration
+if not PNG_BUFFERSIZE.is_integer():
+    raise ValueError("Total Obj Evals must be divisible by 8*BATCH_SIZE")
+else:
+    PNG_BUFFERSIZE = int(PNG_BUFFERSIZE)
+    print("PNG_BUFFERSIZE: ", PNG_BUFFERSIZE)
+
+def anytime_archive_visualizer(self, archive, obj_flag=False, acq_flag=False, pred_flag=False, new_flag=False):
+
     domain = self.domain
     initial_seed = self.initial_seed
 
-    print(f"anytime_archive_visualizer(): iteration: {iteration}")
+    if obj_flag:
+        prefix = "obj"
+        name = "Objective Archive"
+        iteration = self.obj_current_iteration
+    if new_flag:
+        prefix = "new"
+        name = "New Objective Elites"
+        iteration = self.new_current_iteration
+    if acq_flag:
+        prefix = "acq"
+        name = "Acquisition Archive"
+        iteration = self.acq_current_iteration
+    if pred_flag:
+        prefix = "pred"
+        name = "Prediction Archive"
+        iteration = self.pred_current_iteration
 
-    if not os.path.exists(f"imgs/{domain}/{initial_seed}"): 
-        os.makedirs(f"imgs/{domain}/{initial_seed}")
+    print(f"anytime_archive_visualizer(): iteration: {iteration}  archive: {prefix}")
 
-    # print current working directory
+    if not os.path.exists(f"imgs/{domain}/{initial_seed}/{prefix}"): 
+        os.makedirs(f"imgs/{domain}/{initial_seed}/{prefix}")
 
-    os.chdir(f"imgs/{domain}/{initial_seed}")
+    os.chdir(f"imgs/{domain}/{initial_seed}/{prefix}")
 
     fig, ax = plt.subplots(figsize=(8, 8))
     grid_archive_heatmap(archive, ax=ax, vmin=0, vmax=5)
     plt.xlabel("X Up")
     plt.ylabel("Z Up")
 
-    plt.title(f"Objective Eval Heatmap: ({domain} , {iteration})")
+    plt.title(f"{name} Heatmap: ({domain} , {iteration})")
     iteration_str = str(iteration).zfill(3)
-    fig.savefig(f"obj_{iteration_str}_{domain}_heatmap.png")
+    fig.savefig(f"{prefix}_{iteration_str}_{domain}_heatmap.png")
     plt.close()
 
-    if iteration == 0:
-        print("condition reached")
-        output_file = f'obj_{initial_seed}_{domain}.mp4'
-        print(f"anytime_archive_visualizer(): cwd: {os.getcwd()}")
-        subprocess.run(f"ffmpeg -framerate 5 -i obj_%03d_{domain}_heatmap.png -r 30 -pix_fmt yuv420p {output_file}", shell=True, input=b'y\n', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    # render images into video
+    if (iteration) % PNG_BUFFERSIZE == 0 and iteration != 0:       
 
-    if (iteration) % PNG_BUFFERSIZE == 0 and iteration != 0:
+        output_filename = f'{prefix}_{initial_seed}_{domain}_{iteration}_heatmap.mp4'
 
-        # render images into video using default buffervid.mp4 name
-        output_file = f'buffer_vid.mp4'
-        subprocess.run(f"ffmpeg -framerate 5 -i obj_%03d_{domain}_heatmap.png -r 30 -pix_fmt yuv420p {output_file}", shell=True, input=b'y\n', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        os.chdir(f"imgs/{domain}/{initial_seed}")
-
-        ffmpeg_combine = [
-            "ffmpeg",
-            "-i", f"buffer_vid.mp4",
-            "-i", f"obj_{iteration}_{domain}_heatmap.mp4",
-            "-filter_complex", "[0:v] [1:v] concat=n=2:v=1 [v]",
-            "-map", "[v]",
-            f"obj_{initial_seed}_{domain}_heatmap.mp4"
-        ]
-
-        subprocess.run(ffmpeg_combine, input=b'y\n', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(f"ffmpeg -framerate 2 -pattern_type glob -i '*.png' -r 30 -pix_fmt yuv420p {output_filename}", shell=True, input=b'y\n', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         subprocess.run("rm *.png", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-        subprocess.run("rm buffer_vid.mp4", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
-        gc.collect()
 
+        # if two buffered videos exist, combine them
+        if iteration % (PNG_BUFFERSIZE*2) == 0:
+
+            file_a = f'{prefix}_{initial_seed}_{domain}_{iteration-PNG_BUFFERSIZE}_heatmap.mp4'
+            file_b = f'{prefix}_{initial_seed}_{domain}_{iteration}_heatmap.mp4'
+            buffervid_b = f'buffervid_{domain}.mp4'
+
+            # rename video b to avoid nameconflict then use combined_vid_b as output
+            subprocess.run(f"mv {file_b} {buffervid_b}", shell=True)
+
+            ffmpeg_combine = [
+                "ffmpeg",
+                "-i", file_a,
+                "-i", buffervid_b,
+                "-filter_complex", "[0:v] [1:v] concat=n=2:v=1 [v]",
+                "-map", "[v]",
+                file_b
+            ]
+
+            subprocess.run(ffmpeg_combine, input=b'y\n', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(f"rm {file_a} {buffervid_b}", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+            gc.collect()
+
+        # if two combined videos exist, combine them
+        if iteration % (PNG_BUFFERSIZE*4) == 0:
+
+            combined_file_a = f'{prefix}_{initial_seed}_{domain}_{iteration-(PNG_BUFFERSIZE*2)}_heatmap.mp4'
+            combined_file_b = f'{prefix}_{initial_seed}_{domain}_{iteration}_heatmap.mp4'
+            buffervid_b = f'buffervid_{domain}.mp4'
+
+            # rename video b to avoid nameconflict then use combined_vid_b as output
+            subprocess.run(f"mv {combined_file_b} {buffervid_b}", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+            # ffmpeg -i acq_0_custom_hybrid_60_heatmap.mp4 -i buffervid.mp4 -filter_complex "[0:v] [1:v] concat=n=2:v=1 [v]" -map "[v]" acq_0_custom_hybrid_90_heatmap.mp4
+            ffmpeg_combine = [
+                "ffmpeg",
+                "-i", combined_file_a,
+                "-i", buffervid_b,
+                "-filter_complex", "[0:v] [1:v] concat=n=2:v=1 [v]",
+                "-map", "[v]",
+                combined_file_b
+            ]
+            subprocess.run(ffmpeg_combine, input=b'y\n', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(f"rm {combined_file_a} {buffervid_b}", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+            gc.collect()
+
+
+        if iteration % (PNG_BUFFERSIZE*8) == 0:
+
+            combined_file_a = f'{prefix}_{initial_seed}_{domain}_{iteration-(PNG_BUFFERSIZE*4)}_heatmap.mp4'
+            combined_file_b = f'{prefix}_{initial_seed}_{domain}_{iteration}_heatmap.mp4'
+            buffervid_b = f'buffervid_{domain}.mp4'
+
+            # rename video b to avoid nameconflict then use combined_vid_b as output
+            subprocess.run(f"mv {combined_file_b} {buffervid_b}", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+            ffmpeg_combine = [
+                "ffmpeg",
+                "-i", combined_file_a,
+                "-i", buffervid_b,
+                "-filter_complex", "[0:v] [1:v] concat=n=2:v=1 [v]",
+                "-map", "[v]",
+                combined_file_b
+            ]
+
+            subprocess.run(ffmpeg_combine, input=b'y\n', stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            subprocess.run(f"rm {combined_file_a} {buffervid_b}", stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=True)
+            gc.collect()
+
+
+    os.chdir(f"../../../..")
+    return
+
+def archive_visualizer(self, archive, prefix, name, min_val, max_val):
+
+    """Visualize single .png heatmap of archive"""
+
+    domain = self.domain
+    initial_seed = self.initial_seed
+
+    if not os.path.exists(f"imgs/{domain}/{initial_seed}"): 
+        os.makedirs(f"imgs/{domain}/{initial_seed}")
+
+    os.chdir(f"imgs/{domain}/{initial_seed}")
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+    grid_archive_heatmap(archive, ax=ax, vmin=min_val, vmax=max_val)
+    plt.xlabel("X Up")
+    plt.ylabel("Z Up")
+
+    plt.title(f"{name} Heatmap: ({domain})")
+    fig.savefig(f"final_{initial_seed}_{domain}_{prefix}_heatmap.png")
+    plt.close()
 
     os.chdir(f"../../..")
+    return
