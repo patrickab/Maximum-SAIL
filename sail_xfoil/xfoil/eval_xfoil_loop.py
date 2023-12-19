4fc7289d08f14379ae5bb51ae57237d327f0eeb2
patrickab <patrick.a.bitzer@gmail.com>
2023-12-19 16:05:47 +0100

    Remove Archive Updates from eval_xfoil_loop

--- sail_xfoil/xfoil/eval_xfoil_loop.py
+++ sail_xfoil/xfoil/eval_xfoil_loop.py
@@ -8,21 +8,9 @@ samples need to be evaluated in a loop
 In order to preserve high performing, non evaluated solutions,
 acquisition/prediction archives are updated under new GP model.
 
-Updating routine differs between UCB and MES
-
     - Allows indices eg [20:22] to be sampled, if 2 samples are left
     - Obj Elites are always stored & rendered in their archive
     - Subsequently the GP model is updated with new data
-
-    In case of UCB or Predictions 
-        1. archives gets cleared 
-        2. obj elites are added
-        3. previous elites are updated under new gp model
-
-    In case of MES
-        1. archive gets cleared
-        2. 40 best obj elites are added  -  (setting obj value to ACQ_MES_MIN_THRESHHOLD*1.001)
-        3. previous elites are updated under new gp model
 """
 
 import os
@@ -57,42 +45,40 @@ def eval_xfoil_loop(self: SailRun, solution_batch, measures_batch, evaluate_pred
     n_errors = 0
     iteration = 0
     n_new_obj_elites = 0
-    remaining_samples = solution_batch.shape[0]
 
     if self.custom_flag and self.obj_current_iteration % 5 == 0 and not evaluate_prediction_archive:
+        """Evaluate global max mean prediction"""
 
         new_x, max_mean = maximize_mean(self.gp_model)
         generate_parsec_coordinates(new_x)
         _, success_index, converged_obj = xfoil(iterations=1)
-        self.obj_archive.add_single(new_x[0], converged_obj, measures=new_x[0,1:3])
-        print(f"Max Mean: {max_mean} - Max Mean Objective: {converged_obj}")
 
         if converged_obj.shape[0] == 1:
+            self.obj_archive.add_single(new_x[0], converged_obj, measures=new_x[0,1:3])
+            print(f"Max Mean: {max_mean} - Max Mean Objective: {converged_obj}")
             self.update_gp_data(new_solutions=new_x, new_objectives=converged_obj)
 
-    while remaining_samples>0:
+    for i in range(0, solution_batch.shape[0], BATCH_SIZE):
 
-        sample_index = iteration*BATCH_SIZE
-        iter_solutions = solution_batch[sample_index:sample_index+BATCH_SIZE]
-        iter_solutions = np.vstack(iter_solutions)
-        n_solutions = iter_solutions.shape[0]
+        i_solutions = solution_batch[i:i+BATCH_SIZE]
+        i_solutions = np.vstack(i_solutions)
+        n_solutions = i_solutions.shape[0]
 
         # evaluate samples & extract converged solutions
-        generate_parsec_coordinates(iter_solutions)
+        _, _ = generate_parsec_coordinates(i_solutions)
         _, success_indices, converged_obj = xfoil(iterations=n_solutions)
 
         if len(success_indices) == 0:
              continue
 
         success_indices = success_indices[:n_solutions]
-        converged_sol = iter_solutions[success_indices]
-        converged_bhv = measures_batch[sample_index:sample_index+BATCH_SIZE][success_indices]
+        converged_sol = i_solutions[success_indices]
+        converged_bhv = measures_batch[i:i+BATCH_SIZE][success_indices]
 
-        remaining_samples -= n_solutions
         i_errors = n_solutions - len(success_indices)
         n_errors += i_errors
 
-        # Insert -1000 for non_converged samples - used for printing results
+        # Insert -1000 for non_converged samples - used for printing results only
         objective_values = np.full(n_solutions, -1, dtype=float)
         objective_values[success_indices] = converged_obj
         new_objectives = np.vstack((new_objectives, np.vstack(objective_values))) if new_objectives.shape[0] != 0 else np.vstack(objective_values)
@@ -116,8 +102,6 @@ def eval_xfoil_loop(self: SailRun, solution_batch, measures_batch, evaluate_pred
         else:
             self.update_archive(candidate_sol=converged_sol, candidate_obj=converged_obj, candidate_bhv=converged_bhv, evaluate_prediction_archive=True)
 
-    # end xfoil loop
-
     if (not evaluate_prediction_archive) and (not self.random_flag):
         self.update_gp_model()
 
@@ -134,53 +118,6 @@ def eval_xfoil_loop(self: SailRun, solution_batch, measures_batch, evaluate_pred
             target_objectives = np.vstack(np.hstack(candidate_targetvalues))
             true_objectives = np.vstack(new_objectives)
             pprint(target_objectives, true_objectives)
-
-    if acq_flag:
-
-        if self.vanilla_flag:
-
-            obj_elite_df = self.obj_archive.as_pandas(include_solutions=True).sort_values(by='objective', ascending=False)
-            self.acq_archive.clear()
-            self.acq_archive.add(obj_elite_df.solution_batch(), obj_elite_df.objective_batch(), obj_elite_df.measures_batch())
-
-        if self.custom_flag:
-
-            print("acq archive size before update: ", self.acq_archive.stats.num_elites)
-
-            obj_elite_df = self.obj_archive.as_pandas(include_solutions=True).sort_values(by='objective', ascending=False)
-            acq_elite_df = self.acq_archive.as_pandas(include_solutions=True).sort_values(by='objective', ascending=False)
-            n_bins = np.prod(self.acq_archive.dims)
-
-            acq_elites_solutions = acq_elite_df.solution_batch()
-            acq_elites_measures = acq_elite_df.measures_batch()
-
-            obj_elites_solutions = obj_elite_df.solution_batch()
-            obj_elites_objectives = obj_elite_df.objective_batch() if self.acq_ucb_flag else np.full(obj_elites_solutions.shape[0], ACQ_MES_MIN_THRESHHOLD*1.001)
-            obj_elites_measures = obj_elite_df.measures_batch()
-
-            self.acq_archive.clear()
-            self.update_archive(candidate_sol=acq_elites_solutions, candidate_bhv=acq_elites_measures, acq_flag=True, niche_restricted_update=True)
-
-            print("acq archive size after update: ", self.acq_archive.stats.num_elites)
-
-            if self.acq_archive.stats.num_elites < 10 or self.acq_ucb_flag:
-                if self.acq_mes_flag:
-                    obj_elite_df = obj_elite_df.head(int(n_bins*0.05))
-                    obj_elite_df = obj_elite_df.sample(n=BATCH_SIZE, random_state=self.current_seed, replace=True)
-                self.acq_archive.add(obj_elites_solutions, obj_elites_objectives, obj_elites_measures)
-
-        print("Best Acq Objective: ", self.acq_archive.as_pandas(include_solutions=True).sort_values(by='objective', ascending=False).head(1).objective_batch())
-        print("Worst Acq Objective: ", self.acq_archive.as_pandas(include_solutions=True).sort_values(by='objective', ascending=False).tail(1).objective_batch())
-
-    if pred_flag:
-
-        obj_elite_df = self.obj_archive.as_pandas(include_solutions=True).sort_values(by='objective', ascending=False)
-        pred_elite_df = self.pred_archive.as_pandas(include_solutions=True).sort_values(by='objective', ascending=False)
-        self.pred_archive.clear()
-
-        # Update prediction elites under new GP
-        self.pred_archive.add(obj_elite_df.solution_batch(), obj_elite_df.objective_batch(), obj_elite_df.measures_batch())
-        self.update_archive(candidate_sol=pred_elite_df.solution_batch(), candidate_bhv=pred_elite_df.measures_batch(), pred_flag=True)
 
     obj_t1 = self.obj_archive.stats.num_elites
     self.convergence_errors = n_errors
