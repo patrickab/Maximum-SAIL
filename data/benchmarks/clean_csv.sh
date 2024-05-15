4dd9658887c306ba43876db9d9117038fa64d205
patrickab <patrick.a.bitzer@gmail.com>
2024-05-15 21:22:34 +0200

    Add benchmark data

---
+++ data/benchmarks/clean_csv.sh
@@ -0,0 +1,13 @@
+mkdir acq_archive
+mv *acq* acq_archive
+mkdir anytime_metrics
+mv *anytime* anytime_metrics
+mkdir obj_archive
+mv *obj* obj_archive
+mkdir error_archive
+mv *error* error_archive
+mkdir evaluated_pred_archive
+mv *evaluated* evaluated_pred_archive
+mkdir pred_archive
+mv *pred_archive.csv pred_archive
+
